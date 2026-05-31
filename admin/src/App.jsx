f5f7bd5eb994c11026b578f6125b51551b9372b9
richard <richardsunday0812@gmail.com>
2026-05-31 18:23:37 +0100

    feat: integrate react-router-dom for routing and add error handling pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
+import "./App.css";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "./pages/Root";
+import { HomePage } from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ admin/src/pages/Root.jsx
@@ -0,0 +1,6 @@
+import { Outlet } from "react-router-dom";
+
+export function RootLayout() {
+  // const navigation = useNavigation();
+  return <Outlet />;
+}
