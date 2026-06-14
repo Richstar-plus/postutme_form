b2a9397cfc17797e8c810bb10ed0bd0c5e9cad17
richard <richardsunday0812@gmail.com>
2026-06-14 15:10:47 +0100

    feat: update dependencies and add ApplicationsChart component
    - Updated react-router-dom to version 7.16.0
    - Added recharts library for charting functionality
    - Created ApplicationsChart component to visualize application data
    - Added corresponding CSS styles for ApplicationsChart
    - Refactored AdminHome component to include ApplicationsChart

--- users/src/admin/pages/HomePage.jsx
+++ users/src/admin/pages/HomePage.jsx
@@ -1,17 +1,48 @@
 import { AdminCard } from "../components/AdminCard";
-import { faUsers, faNairaSign, faBookBookmark, faCircleCheck    } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUsers,
+  faNairaSign,
+  faBookBookmark,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import ApplicationsChart from "../components/ApplicationsChart";
 export function AdminHome() {
   return (
-    <section className="admin-card">
-      <h2>Welcome to the admin panel</h2>
-      <div className="card-holder">
-        <AdminCard title="Total Applications" price="3,834" image={faUsers} className="first" />
-        <AdminCard title="Total Revenue" price="48,834" image={faNairaSign} className="second" />
+    <>
+      <section className="admin-card">
+        <h2>Welcome to the admin panel</h2>
+        <div className="card-holder">
+          <AdminCard
+            title="Total Applications"
+            price="3,834"
+            image={faUsers}
+            className="first"
+          />
+          <AdminCard
+            title="Total Revenue"
+            price="48,834"
+            image={faNairaSign}
+            className="second"
+          />
 
-        <AdminCard title="Pending Applications" price="3,834" image={faBookBookmark} className="third" />
+          <AdminCard
+            title="Pending Applications"
+            price="3,834"
+            image={faBookBookmark}
+            className="third"
+          />
 
-        <AdminCard title="Completed" price="3,834" image={faCircleCheck} className="fourth" />
-      </div>
-    </section>
+          <AdminCard
+            title="Completed"
+            price="3,834"
+            image={faCircleCheck}
+            className="fourth"
+          />
+        </div>
+      </section>
+      <section className="admin-card">
+        <ApplicationsChart />
+      </section>
+    </>
   );
 }
