3151a5f45a335431016c9dcf60cdaa753964651b
richard <richardsunday0812@gmail.com>
2026-06-14 14:55:43 +0100

    feat: create AdminCard component for displaying admin statistics and update HomePage to utilize it

--- users/src/admin/pages/HomePage.jsx
+++ users/src/admin/pages/HomePage.jsx
@@ -1,10 +1,17 @@
+import { AdminCard } from "../components/AdminCard";
+import { faUsers, faNairaSign, faBookBookmark, faCircleCheck    } from "@fortawesome/free-solid-svg-icons";
 export function AdminHome() {
   return (
     <section className="admin-card">
       <h2>Welcome to the admin panel</h2>
-      <p>
-        Use the navigation menu to access different sections of the admin panel.
-      </p>
+      <div className="card-holder">
+        <AdminCard title="Total Applications" price="3,834" image={faUsers} className="first" />
+        <AdminCard title="Total Revenue" price="48,834" image={faNairaSign} className="second" />
+
+        <AdminCard title="Pending Applications" price="3,834" image={faBookBookmark} className="third" />
+
+        <AdminCard title="Completed" price="3,834" image={faCircleCheck} className="fourth" />
+      </div>
     </section>
   );
 }
